698e66e3a13d022c41ba988c1429a91b501f1633
Andrey Stepanov <isteep@gmail.com>
2015-02-08 21:46:17 +0700

    GetActiveOrders Message

---
+++ message/GetActiveOrdersMessage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace stp\spsr\message;
+
+use stp\spsr\response\Order;
+
+/**
+ * Просмотр списка активных заказов на сбор.
+ *
+ * Позволяет получить список активных заказов на сбор.
+ *
+ * @property string $ICN
+ * @property string $Login
+ */
+class GetActiveOrdersMessage extends BaseMessage
+{
+    public function getRoot()
+    {
+        return 'ActiveOrders';
+    }
+
+    /**
+     * Return XML method name e.g. DataEditManagment/CreateOrder
+     * @return string
+     */
+    public function getMethodString()
+    {
+        return 'DataEditManagment/GetActiveOrders';
+    }
+
+    /**
+     * @param \SimpleXMLElement $response
+     * @return \stp\spsr\response\Order[]
+     */
+    public function buildResponse(\SimpleXMLElement $response)
+    {
+        $result = [];
+        $root = $this->getRoot();
+        foreach($response->Orders->Order as $order) {
+            $attr = (array)$order->attributes();
+            $result[] = new Order($attr['@attributes']);
+        }
+
+        return $result;
+    }
+
+}
---
+++ response/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace stp\spsr\response;
+
+/**
+ * Информация по заказу
+ *
+ * @property string $Order_ID
+ * @property string $Order_Owner_ID
+ * @property string $OrderNumber
+ * @property string $OrderState
+ * @property string $DateOfCreate
+ * @property string $PlaningDT_To
+ * @property string $FIO
+ * @property string $CityName
+ * @property string $Address
+ */
+class Order extends BaseResponse
+{
+
+}
