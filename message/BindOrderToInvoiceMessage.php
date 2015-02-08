5b5198226924aaf6b221a82766c28ff4a8d02957
Andrey Stepanov <isteep@gmail.com>
2015-02-08 21:14:28 +0700

    BindOrderToInvoice Message

---
+++ message/BindOrderToInvoiceMessage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace stp\spsr\message;
+
+/**
+ * Метод позволяет сделать связь между активным заказом и накладными.
+ *
+ * @property string $ICN
+ * @property string $Login
+ * @property string $InvoiceNumber номер накладной в ИС «СПСР-Экспресс»
+ * @property int $Order_ID идентификатор активного заказа (см GetActiveOrders)
+ * @property int $Order_Owner_ID идентификатор активного заказа (см GetActiveOrders)
+ */
+class BindOrderToInvoiceMessage extends BaseMessage
+{
+    public function getRoot()
+    {
+        return 'Invoice';
+    }
+
+    /**
+     * Return XML method name e.g. DataEditManagment/CreateOrder
+     *
+     * @return string
+     */
+    public function getMethodString()
+    {
+        return 'DataEditManagment/BindOrderToInvoice';
+    }
+
+}
